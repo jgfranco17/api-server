e8cfc41984a45c7dbd4fbb2fe4112905767f28a0
Chino Franco <chino.franco@gmail.com>
2022-11-28 20:50:17 +0900

    Updated API title-setting

--- webserver/models/server.py
+++ webserver/models/server.py
@@ -1,13 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 from .data import Payload, Resource
 
 
-def create_app(server_name:str="SERVER"):
-    app = FastAPI()
+def create_app(name:str="SERVER"):
+    app = FastAPI(
+        title=f'{name.upper()} API',
+        description="Central server for core server",
+    )
 
     @app.get("/")
     async def root():
-        return {"message": f'Welcome to {server_name}'}
+        return {"message": f'Welcome to {name} server!'}
+    
+    @app.get("/get-item/{item_id}")
+    async def get_item(item_id:int=Path(None, description="ID of target.")):
+        return 0
 
     @app.post("/add-data")
     async def add_data(payload:Payload=None):
